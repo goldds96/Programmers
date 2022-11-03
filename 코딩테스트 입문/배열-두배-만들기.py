54d260dd72ba78060fc0f76c059f7c6e2ae0cecd
goldds96 <86287862+goldds96@users.noreply.github.com>
2022-11-03 13:54:54 +0900

    Add files via upload

---
+++ 코딩테스트 입문/배열-두배-만들기.py
@@ -0,0 +1,2 @@
+def solution(numbers: list) -> list :
+    return [x*2 for x in numbers]
--- 코딩테스트 입문/분수의-덧셈.py
+++ 코딩테스트 입문/분수의-덧셈.py
@@ -1,2 +1,19 @@
-def solution(denum1: int, num1: int, denum2: int, num2: int) -> array :
-    
+import math
+
+def lcm(a: int, b: int) -> int :
+    return (a * b) // math.gcd(a,b)
+
+def solution(denum1: int, num1: int, denum2: int, num2: int) -> list :
+    answer = []
+    if num1 == num2:
+        answer.append(denum1 + denum2)
+        answer.append(num1)
+    else:
+        mylcm = lcm(num1, num2)
+        answer.append((mylcm // num1) * denum1 + (mylcm // num2) * denum2)
+        answer.append(mylcm)
+    while(math.gcd(answer[0], answer[1]) != 1):
+        divisor = math.gcd(answer[0], answer[1])
+        answer[0] = answer[0] // divisor
+        answer[1] = answer[1] // divisor
+    return answer
